a6de5d16663d28c1a36faef68c2fe51d05e1c8fa
KalaRohit <59739808+KalaRohit@users.noreply.github.com>
2024-04-02 00:34:04 -0500

    basic application complete (#2)

---
+++ Backend/Basic Streaming Gemini App/basic_gemini_app.py
@@ -0,0 +1,27 @@
+import os
+import google.generativeai as genai
+
+def main():
+    quit_symbol = ':q'
+    API_KEY = os.getenv("GEMINI_API_KEY", "")
+    model = genai.GenerativeModel('gemini-pro')
+    genai.configure(api_key=API_KEY)
+
+    while True:
+        user_input = input("Enter text: ")
+
+        if user_input == ":q":
+            return
+        
+        print(f"User input is: {user_input}")
+        
+        response = model.generate_content(
+            user_input,
+            stream=True
+        )
+
+        for chunk in response:
+            print(chunk.text)
+
+if __name__ == "__main__":
+    main()

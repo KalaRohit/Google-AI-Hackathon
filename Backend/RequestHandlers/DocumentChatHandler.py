84d2c8874dc4aa33c8f8915480caa768a7eb1179
Rohit Kala <kalarohit9@gmail.com>
2024-04-05 17:04:36 -0500

    Completed chat

--- Backend/RequestHandlers/DocumentChatHandler.py
+++ Backend/RequestHandlers/DocumentChatHandler.py
@@ -1,6 +1,12 @@
+import google.generativeai as genai 
+from Datamodels.docchat_request import DocumentChatRequest
+from Datamodels.Message import Message
+
 class DocumentChatHandler:
     def __init__(self, document: bytes, api_key: str) -> None:
-        pass
+        self.api_key = api_key
+        self.document = document
+        
     
     def parse_document(self):
         pass
@@ -11,10 +17,21 @@ class DocumentChatHandler:
     def generate_context_for_question(self):
         pass
     
-    def chat(self):
-        # Generator func for sure
-        print("Pretend Chat Message")
+    def chat(self, request: DocumentChatRequest):
+        gemini_compaitible_history = []
+         
+        for message in request.history:
+            gemini_compaitible_history.append(message.get_gemini_format())
         
-        return "fasjlkfasjl"
+        message = \
+            Message(role="user", parts=[request.new_question]).get_gemini_format()
+        gemini_compaitible_history.append(message)
+        genai.configure(api_key=self.api_key, transport="grpc")    
+        model = genai.GenerativeModel('gemini-pro')
+        response = model.generate_content(
+            gemini_compaitible_history
+        )
+        
+        return response.text
     
     

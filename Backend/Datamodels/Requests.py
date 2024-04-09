d913fa8a2b6c9e47c79e2bb9b0df291e5c17bd42
Rohit Kala <kalarohit9@gmail.com>
2024-04-08 20:04:24 -0500

    basic version of embeddings complete

---
+++ Backend/Datamodels/Requests.py
@@ -0,0 +1,25 @@
+from typing import List
+from Datamodels.Message import Message
+from pydantic import BaseModel
+
+class DocumentChatRequest(BaseModel):
+    chat_id: str
+    document_id: str
+    history: List[Message]
+    new_question: str
+    
+    
+    def get_formatted_history(self) -> List[dict]:
+        gemini_compaitible_messages = []
+        for m in self.history:
+            gemini_compaitible_messages.append(
+                m.get_gemini_format()
+            )
+    
+        return gemini_compaitible_messages
+    
+class SummarizeRequest(BaseModel):
+    request_id: str
+    text: str
+    target_reading_level: int
+    
--- Backend/RequestHandlers/DocumentChatHandler.py
+++ Backend/RequestHandlers/DocumentChatHandler.py
@@ -1,69 +1,46 @@
+import os
+
 import google.generativeai as genai 
-from Datamodels.docchat_request import DocumentChatRequest
-from Datamodels.Message import Message
-from vertexai.language_models import TextEmbeddingModel
 from qdrant_client import QdrantClient
 from qdrant_client.models import Distance, VectorParams, PointStruct
-from io import BytesIO
-from typing import List
-import PyPDF2
+from vertexai.language_models import TextEmbeddingModel
+import yaml
+
+from Datamodels.Requests import DocumentChatRequest
+# from Datamodels.Responses import DocumentUploadResponse
+from Datamodels.Message import Message
+import pprint
 
 class DocumentChatHandler:
-    def __init__(self, document: bytes, api_key: str, document_name: str) -> None:
-        self.api_key = api_key
-        self.document = document
-        self.document_name = document_name
+    def __init__(self, request: DocumentChatRequest) -> None:
+        self.request = request
         
-    async def parse_document(self, n_size = 300):
-        pdf = BytesIO(self.document)
-        pdf_reader = PyPDF2.PdfReader(pdf)
-        
-        for page in pdf_reader.pages:
-            text = page.extract_text()
-            for i in range(0, len(text), n_size):
-                chunk = text[i:i + n_size]
-                chunk_string = str(chunk)
-                print(chunk_string)
-                yield chunk_string
     
-    
-    async def embed_text(self):
-        print("Embedding Text...")
+    def get_relevant_context_from_vector_store(self, document_id: str, search_query: str) -> str:
+        """
+        Query a vector store to get some relevant context on a search query
         
-        embedding_model = TextEmbeddingModel.from_pretrained("textembedding-gecko@001")
-        async for page_text in self.parse_document():
-            embeddings = embedding_model.get_embeddings([page_text])
-            vector = embeddings[0].values
+        Args: 
         
-            yield vector, page_text
+            document_id: str
+                The name/id of the document
+            search_query: str
+                The search query is the string which will first be embedded using
+                the textembedding-gecko@001, which is then used to perform
+                ANN search using QDrantDB.
         
-    async def one_shot_embed(self):
-        print("Embedding Collection...")
+        Returns:
+            str
+                The context fetched from vector store.
+        """
         qdrant_client = QdrantClient(url="http://localhost:6333")
-        qdrant_client.create_collection(
-            collection_name=self.document_name,
-            vectors_config=VectorParams(size=768, distance=Distance.COSINE)
-        )
-
-        
-        
-        index = 0
-        async for vector, text in self.embed_text():
-            qdrant_client.upsert(
-                collection_name=self.document_name,
-                points=[PointStruct(id=index, vector=vector, payload={"key": text})]
-            )
-            
-            index += 1
-               
-    async def generate_context_for_question(self, question: str):
-        print("Context Collection...")
-        qdrant_client = QdrantClient(url="http://localhost:6333")
+        print(search_query)
         embedding_model = TextEmbeddingModel.from_pretrained("textembedding-gecko@001")
-        embedded_question = embedding_model.get_embeddings([question])[0].values
+        embedded_question = embedding_model.get_embeddings([search_query])[0].values
         search_result = qdrant_client.search(
-            collection_name=self.document_name,
-            query_vector=embedded_question
+            collection_name=document_id,
+            query_vector=embedded_question,
+            limit=3
         )
         
         context = ""
@@ -73,31 +50,51 @@ class DocumentChatHandler:
             
             context += payload
         
-        return context
+        print(context.replace("\\n", ""))
         
+        return context.replace("\\n", "")
     
-    async def chat(self, request: DocumentChatRequest):
-        gemini_compaitible_history = []
-         
-        for message in request.history:
-            gemini_compaitible_history.append(message.get_gemini_format())
-        print("One shot embedding...")
+    def summarize_text(self, text: str) -> str:
+        """
         
-        context = await self.generate_context_for_question(question=request.new_question)
+        """
+        print("In summarize text...")
+        API_KEY = os.getenv("GEMINI_API_KEY", "")
+        model = genai.GenerativeModel(
+            'gemini-pro'
+        )
+        genai.configure(api_key=API_KEY)
         
-        request.new_question += f"Additional information to help you answer this question: {context}"
+        summary = model.generate_content(text)
         
-        print(request.new_question)
-        message = \
-            Message(role="user", parts=[request.new_question]).get_gemini_format()
-        gemini_compaitible_history.append(message)
-        genai.configure(api_key=self.api_key, transport="grpc")    
-        model = genai.GenerativeModel('gemini-pro')
+        return summary
+    
+    def model_chat(self):
+        messages = self.request.get_formatted_history()
+        pprint.pprint(messages)
+        API_KEY = os.getenv("GEMINI_API_KEY", "")
+        model = genai.GenerativeModel(
+            'gemini-pro', 
+            tools=[self.get_relevant_context_from_vector_store, self.summarize_text]
+        )
+        genai.configure(api_key=API_KEY, transport='rest')
         
-        response = model.generate_content(
-            gemini_compaitible_history
+        with open("./prompts/gemini-pro.yml", 'r') as readfile:
+            prompt: str = yaml.safe_load(readfile)["document-chatter-prompt"]
+            prompt = prompt.format(
+                document_id=self.request.document_id,
+                user_query=self.request.new_question
+            )
+        
+        print(prompt)
+        chat = model.start_chat(history=messages, 
+            enable_automatic_function_calling=True
+        )
+        
+        response = chat.send_message(
+            prompt
         )
         
         return response.text
-    
-    
+        
+        

69c20c9b90f6bf0e539570ac42cae3a946c1552d
KalaRohit <59739808+KalaRohit@users.noreply.github.com>
2024-04-25 13:29:32 -0700

    26 Frontend Skeleton Setup (#36)
    * added paragraph test
    
    * added switch and drop down and connected to reading levels
    
    * fixed values
    
    * formatting
    
    * re-added background.js
    
    ---------
    
    Co-authored-by: katherine bec <katherinebec@Katherines-Mac.local>

--- frontend/content.js
+++ frontend/content.js
@@ -1,10 +1,17 @@
-// Receive message from the popup
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
-    if(request.message == "switch_text"){
-        // Only <p> elements will be simplified
-        document.querySelectorAll('p').forEach(switchText);
+
+let request = null; 
+chrome.runtime.onMessage.addListener(
+        function(req, sender, sendResponse){ 
+            request = req; console.log(request); 
+            if(request.message === "switch_text"){ 
+                console.log('switching text!'); 
+            document.querySelectorAll('p').forEach(switchText); 
+            } 
+            if (request.message === "change_grade"){ 
+                console.log(request.grade); 
+            } 
     }
-})
+)
 
 async function switchText(p) {
     // pass p to backend, and get the model output
@@ -19,11 +26,6 @@ async function simplifyText(p) {
         "text": p,
         "target_reading_level": 3
     }
-
-    // const repsonse = await fetch("http://gai.hackathon.rohitkala.com/backend/v1/model/gemini-pro:summarize", {
-    //     method: "POST",
-    //     body: JSON.stringify(request_obj)
-    // });
 
     const repsonse = await fetch("https://catfact.ninja/fact", {
         method: "GET"
--- frontend/index.js
+++ frontend/index.js
@@ -1,12 +1,25 @@
 document.addEventListener("DOMContentLoaded", runFunction);
-
 function runFunction() {
-    let button = document.getElementById("primary-button");
-    button.addEventListener("click", changeBgColor);
-    function changeBgColor(){
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-            // Send a message to the current tab
-            chrome.tabs.sendMessage(tabs[0].id, {message: "switch_text"});
-        })
-    }
+  let checkbox = document.getElementById("flexSwitchCheckDefault");
+  let select = document.getElementById("ReadingLevel");
+  select.addEventListener("change", outputSelection);
+  checkbox.addEventListener("click", changeBgColor);
+  function changeBgColor() {
+    chrome.tabs.query(
+      { active: true, currentWindow: true },
+
+      function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { message: "switch_text" });
+      }
+    );
+  }
+  function outputSelection() {
+    var selectedOption = document.getElementById("ReadingLevel").value;
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {
+        message: "change_grade",
+        grade: selectedOption,
+      });
+    });
+  }
 }
